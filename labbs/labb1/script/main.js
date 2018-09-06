007e919cf3c7c501542407ad9ffd81ebb4fb6310
Jimmy <jimbj685@student.liu.se>
2018-09-06 12:20:44 +0200

    Fix it for me

--- labbs/labb1/script/main.js
+++ labbs/labb1/script/main.js
@@ -5,6 +5,7 @@ function submit(){
 $(document).ready(function() {
     $("#post_dummy").hide();
     var amount = 0;
+
     $("#submit").click(function(){
         var current_time = new Date($.now());
         var text = $("#text_post").val();
@@ -12,17 +13,37 @@ $(document).ready(function() {
         
         if (text.length > 0){
             if(text.length <= 140){
-                $("#articles").prepend(`<article id='post_${amount}'><p id='post_${amount}'>${text}</p><form><span id='read_undread' value=0>Unread</span><input type='checkbox'></form></article>`);
+                $("#articles").prepend(`<article id='post_${amount}'><p id='post_${amount}_text'>${text}</p><form><span id='read_${amount} ' value='0'>Unread</span><input type='checkbox' id='read_box_${amount}' class='read_box'></form></article>`);
+                var text = $("#text_post").val("");
+                $(".read_box").change(function(){
+                    click_read(this);
+                });
             }else{
                 $("#error_span").html("Your message is too long!");
             };
         }else{
             $("#error_span").html("Your message is too short!");
-        }
+        };
 
-        var text = $("#text_post").val("");
     });
+
+
 });
+
+function click_read(current){
+    var number = $(current).attr("id").split("_")[2];
+    alert("test");
+    if($("read_"+number).attr("value") == '0'){
+        alert("Hej");
+        $("read_"+number).text("Read");
+        alert("hej2");
+        $("read_"+number).attr("value", '1');
+    }else{
+        alert("test2");
+        $("read_"+number).text("Unread");
+        $("read_"+number).attr("value", '0');
+    };
+};
 
 
 
