729162d8f2badf12c8d837f4f3ea99bc2447eb3f
Jimmy <jimbj685@student.liu.se>
2018-09-11 11:36:38 +0200

    Created Venv and installed Selenium

--- labbs/labb1/script/main.js
+++ labbs/labb1/script/main.js
@@ -5,17 +5,16 @@ function submit(){
 $(document).ready(function() {
     $("#post_dummy").hide();
     var amount = 0;
-
     $("#submit").click(function(){
         var current_time = new Date($.now());
         var text = $("#text_post").val();
-
-
         if (text.length > 0){
             if(text.length <= 140){
                 $("#articles").prepend(`<article id='post_${amount}'><p id='post_${amount}_text' class="post_text">${text}</p><span id='read_${amount}'>Unread</span><form><input type='checkbox' id='read_box_${amount}' class='read_box'></form></article>`);
-                var text = $("#text_post").val("");
-                $(".read_box").change(function(){
+                $("#text_post").val("");
+                
+                $(".read_box").unbind();
+                $(".read_box").click(function(){
                     click_read(this);
                 });
                 amount = amount + 1;
@@ -25,18 +24,16 @@ $(document).ready(function() {
         }else{
             $("#error_span").html("Your message is too short!");
         };
-
     });
-
-
 });
 
 function click_read(current){
     var number = $(current).attr("id").split("_")[2];
-    alert(number);
     if($("#read_"+number).text() == 'Unread'){
         $("#read_"+number).text("Read");
+        $("#post_"+number).css("background-color", "green")
     }else{
         $("#read_"+number).text("Unread");
+        $("#post_"+number).css("background-color", "lightblue")
     };
 };
