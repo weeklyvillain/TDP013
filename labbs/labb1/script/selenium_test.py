51fa636d12cb5c8058bf1fd2ba95f36cc2c128ad
Jimmy <jimbj685@student.liu.se>
2018-09-19 13:27:46 +0200

    Fixed problem with selenium opening multiple web browser drivers

--- labbs/labb1/script/selenium_test.py
+++ labbs/labb1/script/selenium_test.py
@@ -6,31 +6,33 @@ class PythonOrgSearch(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.driver = webdriver.Firefox()
+    
+    @classmethod
+    def tearDownClass(self):
+        self.driver.close()
 
     def test_open_page(self):
         #Testing whether page is open
-        driver = self.driver
-        driver.get("file:///home/spathatos/Projects/TDP013/labbs/labb1/version_jquery.html")
-        self.assertIn("Wittier", driver.title)
+        
+            self.driver.get("file:///home/spathatos/Projects/TDP013/labbs/labb1/version_jquery.html")
+            self.assertIn("Wittier", self.driver.title)
+        
         #Testing whether we can write to the text box
-        text_box = driver.find_element_by_id("text_post")
-        text_box.send_keys("Hello World!")
-        self.assertEqual("Hello World!", text_box.get_attribute("value"))
+            text_box = self.driver.find_element_by_id("text_post")
+            text_box.send_keys("Hello World!")
+            self.assertEqual("Hello World!", text_box.get_attribute("value"))
 
     def test_send_posts(self):
-        driver = self.driver
-        driver.get("file:///home/spathatos/Projects/TDP013/labbs/labb1/version_jquery.html")
-        text_box = driver.find_element_by_id("text_post")
+        self.driver.get("file:///home/spathatos/Projects/TDP013/labbs/labb1/version_jquery.html")
+        text_box = self.driver.find_element_by_id("text_post")
         text_box.send_keys("Hello World!")
         self.assertEqual("Hello World!", text_box.get_attribute("value"))    
         text_box.send_keys(Keys.RETURN)
-        self.assertEqual(len(driver.find_elements_by_id("post_0")), 1)
+        self.assertEqual(len(self.driver.find_elements_by_id("post_0")), 1)
         
-        submit = driver.find_element_by_id("submit")
+        submit = self.driver.find_element_by_id("submit")
         
 
-    def tearDown(self):
-        self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
