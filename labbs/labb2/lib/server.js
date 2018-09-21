9d11f3183b8a8cbd5aed227fa76ebd8ab0ded0ad
Jimmy <jimbj685@student.liu.se>
2018-09-21 09:35:20 +0200

    Installed Mongodb and added tdp013 database, also refactord main.js

--- labbs/labb2/lib/server.js
+++ labbs/labb2/lib/server.js
@@ -3,29 +3,38 @@ var express = require('express');
 function start() {
     var app = express(); 
 
-    app.get('/', function (req, res) { 
+    app.get("/index.html", function (req, res, next){ 
+        res.redirect("/");  
+    }); 
+
+    app.get('/', function (req, res, next) { 
         res.send(`<!doctype html>
         <html>
         <body>
-            <form action='/add_message' method='post'>
+            <form action='/save' method='get'>
                 <input type='text' name='fname' /><br />
                 <input type='number' name='age' /><br />
-                <input type='file' name='photo' /><br />
+                <input type='file' name='photo'  /><br />
                 <input type="submit" value="Send Request">
             </form>
         </body>
         </html>`); 
     }); 
 
-    app.get("/index.html", function (req, res){ 
-        res.redirect("/"); 
-        
-    }); 
-
-    app.post('/add_message', function(req, res, next){ 
-        console.log(req.body['fname']);
+    app.get('/save', function(req, res, next){ 
+        console.log(req.body['post_body']);
         console.log("Adding message!");
-        //...
+    });
+
+    app.get('/flag', function(req, res, next){
+        console.log(req.body['id']);
+        console.log("Flagging message!");
+    });
+
+    app.get('/getall', function(req, res, next){
+
+        console.log("Getting all messages!");
+        res.status(200).send("post1");
     });
 
     app.delete("*", function(req, res){
