d96fd9a8151eda0758c4e01b59d6f184fc565555
Jimmy <jimbj685@student.liu.se>
2018-10-25 11:28:21 +0200

    Removed unused files

--- project/client/src/pages.js
+++ project/client/src/pages.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from './login';
 import Profile from './profile_page';
-import Search from './search';
 
 
 const Pages = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Login} />
-      <Route path='/search' component={Search} />
       <Route path='/:id' component={Profile} />
       </Switch>
   </BrowserRouter>
